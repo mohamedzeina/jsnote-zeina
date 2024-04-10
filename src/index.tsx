f82568df5e8f2b681b8d3cbb4872783e53f84fb3
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-10 22:02:26 +0300

    replaced service variable with a ref to use the ESbuild object outside of the startService function

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
-import { useState, useEffect } from 'react';
-import { start } from 'repl';
+import { useState, useEffect, useRef } from 'react';
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm',
     });
-    console.log(service);
+    console.log(ref.current);
   };
 
   useEffect(() => {
@@ -20,7 +20,10 @@ const App = () => {
   }, []); //second argument ([]) just states that this function will be called only once when the page renders
 
   const onClick = () => {
-    console.log(input);
+    if (!ref.current) {
+      return;
+    }
+    console.log(ref.current);
   };
 
   return (
