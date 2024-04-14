0fa197f32f5dd9cf3690dd2e3cda42a34c72b836
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-14 04:10:40 +0300

    code execution inside iframe using srcDoc added

--- src/index.tsx
+++ src/index.tsx
@@ -43,6 +43,12 @@ const App = () => {
     setCode(result.outputFiles[0].text);
   };
 
+  const html = `
+  <script> 
+    ${code} 
+  </script>
+  `;
+
   return (
     <div>
       <textarea
@@ -53,13 +59,10 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox="" srcDoc={html}></iframe>
+      <iframe sandbox="allow-scripts" srcDoc={html}></iframe>
     </div>
   );
 };
-
-const html = `
-<h1>Local HTML doc </h1>`;
 
 root.render(<App />);
 
