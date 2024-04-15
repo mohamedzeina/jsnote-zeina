d8c921b963f3b838b24ea7b366b077f93f74f1cd
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-15 15:45:14 +0300

    added a few more configurations to the editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -8,6 +8,13 @@ const codeEditor = () => {
       height="500px"
       options={{
         wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false, // Do not fade out import statements that are not used straight away
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
       }}
     />
   );
