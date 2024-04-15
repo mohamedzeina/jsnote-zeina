94357a3cd60217b02e0aa5e1fe6ebf1f4814a619
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-15 15:40:16 +0300

    added word wrapping option to editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,16 @@
 import MonacoEditor from '@monaco-editor/react';
 
 const codeEditor = () => {
-  return <MonacoEditor theme="dark" language="javascript" height="500px" />;
+  return (
+    <MonacoEditor
+      theme="dark"
+      language="javascript"
+      height="500px"
+      options={{
+        wordWrap: 'on',
+      }}
+    />
+  );
 };
 
 export default codeEditor;
