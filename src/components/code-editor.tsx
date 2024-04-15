f1a510bbd79501ce5d66f71bf64fea80fde94640
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-15 15:26:31 +0300

    added basic configuration to the editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,7 @@
 import MonacoEditor from '@monaco-editor/react';
 
 const codeEditor = () => {
-  return <MonacoEditor height="500px" />;
+  return <MonacoEditor theme="dark" language="javascript" height="500px" />;
 };
 
 export default codeEditor;
