76dcbb069f1166ecacabe6aa90280393d047bdc3
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-16 01:20:08 +0300

    added basic debouncing logic for code execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,18 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
 import Resizable from './resizable';
-import { ResizableBox } from 'react-resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
+  }, [input]);
 
   return (
     <Resizable direction="vertical">
