b08de665cb6ba5ef3eba6e2d103fc83e43007ec4
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-16 01:14:30 +0300

    synchronized width state in resizable component and reizeableBox element to fix width jumps

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -11,16 +11,21 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
   const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
 
   useEffect(() => {
     let timer: any;
     const listener = () => {
+      // debouncing
       if (timer) {
         clearTimeout(timer);
       }
       timer = setTimeout(() => {
         setInnerHeight(window.innerHeight);
         setInnerWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75); // fixing bug with max constraint when resizing brower's width
+        }
       }, 100);
     };
     window.addEventListener('resize', listener);
@@ -36,8 +41,11 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
       minConstraints: [innerWidth * 0.2, Infinity],
       maxConstraints: [innerWidth * 0.75, Infinity], // resize box cannot go beyond 75% of the width of the screen
       height: Infinity,
-      width: innerWidth * 0.75,
+      width,
       resizeHandles: ['e'],
+      onResizeStop: (event, data) => {
+        setWidth(data.size.width);
+      },
     };
   } else {
     resizableProps = {
