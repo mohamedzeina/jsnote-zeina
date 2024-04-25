4cd98780775b124339fa8a5ae0cc4bc3d74799eb
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-25 20:13:06 +0300

    implemented move_cell in the reducer with immer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -23,17 +23,28 @@ const reducer = produce((state: CellState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
-
       state.data[id].content = content; // immer allows us to make these simple updates
-      return;
 
+      return;
     case ActionType.DELETE_CELL:
       delete state.data[action.payload]; // deleting cell from data dictionary
       state.order = state.order.filter((id) => id !== action.payload); // delete cell from the order array
+
+      return;
+    case ActionType.MOVE_CELL:
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1; // new index of cell after moving depending on direction
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id; // basic swap operation between the two cells
+
       return;
     case ActionType.INSERT_CELL_BEFORE:
-      return state;
-    case ActionType.MOVE_CELL:
       return state;
     default:
       return state;
