c0557df1601b51c4f5226e14fafda080cbc6e832
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-25 19:10:43 +0300

    added action creator return types

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,7 +1,7 @@
 import { ActionType } from "../action-types";
 import { CellTypes } from "../cell";
 
-interface MoveCellAction {
+export interface MoveCellAction {
   type: ActionType.MOVE_CELL;
   payload: {
     id: string;
@@ -10,12 +10,12 @@ interface MoveCellAction {
 
 }
 
-interface DeleteCellAction {
+export interface DeleteCellAction {
   type: ActionType.DELETE_CELL;
   payload: string;
 }
 
-interface InsertCellBeforeAction {
+export interface InsertCellBeforeAction {
   type: ActionType.INSERT_CELL_BEFORE;
   payload: {
     id: string; // id of the cell we want to insert the new cell before
@@ -23,7 +23,7 @@ interface InsertCellBeforeAction {
   }
 }
 
-interface UpdateCellAction {
+export interface UpdateCellAction {
   type: ActionType.UPDATE_CELL;
   payload: {
     id: string;
