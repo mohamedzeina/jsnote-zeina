af5b49417f93b2d1cf1c9fca5419f379ba2ab63b
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-25 19:02:42 +0300

    connected react app to redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './state';
 import TextEditor from './components/text-editor';
 
 const el = document.getElementById('root');
@@ -7,9 +9,11 @@ const root = ReactDOM.createRoot(el!);
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
