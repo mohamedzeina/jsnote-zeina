f0dd0c20faf42756349c4f0b253d6ceb5d0fda17
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-29 20:53:10 +0300

    added manual testing code for redux store

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -20,30 +20,31 @@ const initialState: CellState = {
 };
 
 const reducer = produce((state: CellState = initialState, action: Action) => {
+  // wrapped it with a produce to use immer
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
       state.data[id].content = content; // immer allows us to make these simple updates
 
-      return;
+      return state;
     case ActionType.DELETE_CELL:
       delete state.data[action.payload]; // deleting cell from data dictionary
       state.order = state.order.filter((id) => id !== action.payload); // delete cell from the order array
 
-      return;
+      return state;
     case ActionType.MOVE_CELL:
       const { direction } = action.payload;
       const index = state.order.findIndex((id) => id === action.payload.id);
       const targetIndex = direction === 'up' ? index - 1 : index + 1; // new index of cell after moving depending on direction
 
       if (targetIndex < 0 || targetIndex > state.order.length - 1) {
-        return;
+        return state;
       }
 
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id; // basic swap operation between the two cells
 
-      return;
+      return state;
     case ActionType.INSERT_CELL_BEFORE:
       const cell: Cell = {
         content: '',
@@ -63,7 +64,7 @@ const reducer = produce((state: CellState = initialState, action: Action) => {
         state.order.splice(foundIndex, 0, cell.id); // adding cell id before the id in the payload
       }
 
-      return;
+      return state;
     default:
       return state;
   }
