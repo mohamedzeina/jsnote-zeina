37379ac27d82a480873432f79c8c3deaa6c0d081
Mohamed Zeina <toxiczeina@gmail.com>
2024-04-29 20:57:28 +0300

    added cell-list and cell-list-item files

---
+++ src/components/cell-list-item.tsx
@@ -0,0 +1,5 @@
+const CellListItem: React.FC = () => {
+  return <div>CellList Item</div>;
+};
+
+export default CellListItem;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './state';
-import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el!);
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
