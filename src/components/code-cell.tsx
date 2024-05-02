e9d3f6cd7ae6a967828b52eb8033ecd9a15c9b06
Mohamed Zeina <toxiczeina@gmail.com>
2024-05-02 15:45:01 +0300

    added eager bundling to fix preview window not showing when app first renders

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -16,6 +16,11 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 1000);
@@ -23,6 +28,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return () => {
       clearTimeout(timer); // clearing timer if user input is updated before 1 second passes
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.id, cell.content, createBundle]);
 
   return (
