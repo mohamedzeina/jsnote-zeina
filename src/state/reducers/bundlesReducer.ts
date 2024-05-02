601cebaa77104d6cd9679c8762462165cd8d804c
Mohamed Zeina <toxiczeina@gmail.com>
2024-05-02 14:41:33 +0300

    added initial implementation of bulder reducer

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -1,1 +1,28 @@
-import { Action } from 'redux';
+import { produce } from 'immer';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+interface BundleState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    err: string;
+  };
+}
+
+const initialState: BundleState = {};
+
+const reduer = produce(
+  (state: BundleState = initialState, action: Action): BundleState => {
+    switch (action.type) {
+      case ActionType.BUNDLE_START:
+        return state;
+      case ActionType.BUNDLE_COMPLETE:
+        return state;
+      default:
+        return state;
+    }
+  }
+);
+
+export default reduer;
