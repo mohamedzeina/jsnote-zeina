8c94693b514a45b6bd0bbcdd279012d9a43b99ce
Mohamed Zeina <toxiczeina@gmail.com>
2024-05-03 23:46:04 +0300

    added serve command line arguments for filename and port

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -3,9 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
 const commander_1 = require("commander");
 exports.serveCommand = new commander_1.Command()
-    .command('serve') // watch for serve command in the cmd
+    .command('serve [filename]') // watch for serve command in the cmd
     .description('Open a file for editing')
-    .action(() => {
+    .option('-p, --port <number>', 'port to run server on', '4005')
+    .action((filename = 'notebook.js', options) => {
     // logic of the command
-    console.log('Getting ready to serve a file');
+    console.log(filename, options);
 });
+/*
+    -[filename]: specifices that there is an optional filename argument
+    that can be provided with the serve command
+    -<number>: specifies that if a user provides a port option
+    they have to provide a port numer
+    -notebook.js is the default file name that is going to save
+    the cells of the user in case the user does not provide any
+    filename
+    
+
+
+*/
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,9 +1,23 @@
 import { Command } from 'commander';
 
 export const serveCommand = new Command()
-  .command('serve') // watch for serve command in the cmd
+  .command('serve [filename]') // watch for serve command in the cmd
   .description('Open a file for editing')
-  .action(() => {
+  .option('-p, --port <number>', 'port to run server on', '4005')
+  .action((filename = 'notebook.js', options) => {
     // logic of the command
-    console.log('Getting ready to serve a file');
+    console.log(filename, options);
   });
+
+/*
+    -[filename]: specifices that there is an optional filename argument
+    that can be provided with the serve command
+    -<number>: specifies that if a user provides a port option
+    they have to provide a port numer
+    -notebook.js is the default file name that is going to save
+    the cells of the user in case the user does not provide any
+    filename
+    
+
+
+*/
