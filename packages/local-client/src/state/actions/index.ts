f3b2c59819ed8325e96badac10cd617bdc9cc652
Mohamed Zeina <toxiczeina@gmail.com>
2024-05-06 17:13:05 +0300

    added fetch cells logic to redux store

--- packages/local-client/src/state/actions/index.ts
+++ packages/local-client/src/state/actions/index.ts
@@ -1,6 +1,5 @@
-import { Bundle } from 'typescript';
 import { ActionType } from '../action-types';
-import { CellTypes, CellDirection } from '../cell';
+import { Cell, CellTypes, CellDirection } from '../cell';
 
 export interface MoveCellAction {
   type: ActionType.MOVE_CELL;
@@ -49,10 +48,25 @@ export interface BundleCompleteAction {
   };
 }
 
+export interface FetchCellsAction {
+  type: ActionType.FETCH_CELLS;
+}
+export interface FetchCellsCompleteAction {
+  type: ActionType.FETCH_CELLS_COMPLETE;
+  payload: Cell[];
+}
+export interface FetchCellsErrorAction {
+  type: ActionType.FETCH_CELLS_ERROR;
+  payload: string;
+}
+
 export type Action =
   | MoveCellAction
   | DeleteCellAction
   | InsertCellAfterAction
   | UpdateCellAction
   | BundleStartAction
-  | BundleCompleteAction;
+  | BundleCompleteAction
+  | FetchCellsAction
+  | FetchCellsCompleteAction
+  | FetchCellsErrorAction;
