9391466be2aaeb94d3ba9ec737ebfec25106e30d
Mohamed Zeina <toxiczeina@gmail.com>
2024-05-06 16:25:26 +0300

    added two cases depending on the execution environment for refrencing the production build

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,22 +5,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
 const path_1 = __importDefault(require("path"));
-const serve = (port, filename, dir) => {
+const serve = (port, filename, dir, useProxy) => {
     const app = (0, express_1.default)();
-    const packagePath = require.resolve('local-client/build/index.html'); // gets absolute path on user's local machine
-    console.log(packagePath);
-    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
-    // app.use(
-    //   createProxyMiddleware({
-    //     target: 'http://127.0.0.1:3000',
-    //     ws: true, //websocket support
-    //   })
-    // );
+    if (useProxy) {
+        app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+            target: 'http://127.0.0.1:3000',
+            ws: true, //websocket support
+        }));
+    }
+    else {
+        const packagePath = require.resolve('local-client/build/index.html'); // gets absolute path on user's local machine
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
     return new Promise((resolve, reject) => {
         app.listen(port, resolve).on('error', reject);
     });
 };
 exports.serve = serve;
-// if express server starts successfully, resolve function is called and promise is finished
-// if something fails, reject function will be called with an error state
+/* if express server starts successfully, resolve function is called and promise is finished
+   if something fails, reject function will be called with an error state
+
+   added useProxy boolean to seperate the two cases where our execution environment is being used
+   case 1: we are in development mode, constantly developing the app, in this case, we want to see
+   the changes done on the app instantly so we use the proxy method
+   case 2: we are in production mode, user won't have access to a current react app running so we
+   reference production build using express static
+*/
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -2,23 +2,36 @@ import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import path from 'path';
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
 
-  const packagePath = require.resolve('local-client/build/index.html'); // gets absolute path on user's local machine
-  console.log(packagePath);
-  app.use(express.static(path.dirname(packagePath)));
-
-  // app.use(
-  //   createProxyMiddleware({
-  //     target: 'http://127.0.0.1:3000',
-  //     ws: true, //websocket support
-  //   })
-  // );
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: 'http://127.0.0.1:3000',
+        ws: true, //websocket support
+      })
+    );
+  } else {
+    const packagePath = require.resolve('local-client/build/index.html'); // gets absolute path on user's local machine
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
   });
 };
-// if express server starts successfully, resolve function is called and promise is finished
-// if something fails, reject function will be called with an error state
+/* if express server starts successfully, resolve function is called and promise is finished
+   if something fails, reject function will be called with an error state
+
+   added useProxy boolean to seperate the two cases where our execution environment is being used
+   case 1: we are in development mode, constantly developing the app, in this case, we want to see
+   the changes done on the app instantly so we use the proxy method
+   case 2: we are in production mode, user won't have access to a current react app running so we 
+   reference production build using express static
+*/
