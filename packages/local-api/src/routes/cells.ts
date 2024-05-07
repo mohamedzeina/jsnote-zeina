f0f14e8b5e4b1c9aa16f9eb32bb78c30e8865855
Mohamed Zeina <toxiczeina@gmail.com>
2024-05-08 01:07:11 +0300

    added default cells for when the user first starts the app for guidance

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -12,6 +12,27 @@ interface LocalApiError {
   code: string;
 }
 
+const defaultCells = [
+  {
+    content:
+      'JSNote-Zeina\n----------\nThis is an interactive coding environment. You can write Javascript, see it executed, and write coprehensive documentation using markdown.\n\n- Click any text cell (including this one) to edit it\n- The code in each code editor is all joined into one file. If you define a variable in cell #1, you can refer to it in any following code cell!\n- You can show any React component, string, number, or anything else by calling the `show `function. This is a function built into this environment. Call show multiple times to show multiple values\n- Re-order or delete cells using the buttons on the top right \n- Add new cells by hovering on the divider between each cell\n\nAll of your changes get saved to the file you opened Jbook with. So if you ran `npx jsnote-zeina serve test.js` , all of the text and code you write will be saved to the `test.js` file.',
+    type: 'text',
+    id: 'ohbrr',
+  },
+  {
+    content:
+      "import { useState } from 'react';\r\nconst Counter = () => {\r\n  const [count, setCount] = useState(0);\r\n  return (\r\n    <div>\r\n      <button onClick={() => setCount(count + 1)}>Click </button>\r\n      <h3>Count: {count} </h3>\r\n    </div>\r\n  );\r\n};\r\n\r\nshow(<Counter />);",
+    type: 'code',
+    id: 'yvorb',
+  },
+  {
+    content:
+      'const App = () => {\r\n  return (\r\n    <div>\r\n      <h3> App says Hello </h3>\r\n      <i> Counter component will be rendered below... </i>\r\n      <hr />\r\n      {/* Counter was declared in an earlier cell -\r\n  We can reference it here! */}\r\n      <Counter />\r\n    </div>\r\n  );\r\n};\r\n\r\nshow(<App />);',
+    type: 'code',
+    id: 'toeeq',
+  },
+];
+
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
   router.use(express.json());
@@ -32,8 +53,12 @@ export const createCellsRouter = (filename: string, dir: string) => {
       if (isLocalApiError(err)) {
         if (err.code === 'ENOENT') {
           // Create a file and add default cells
-          await fs.writeFile(fullPath, '[]', 'utf-8');
-          res.send([]);
+          const newFile = await fs.writeFile(
+            fullPath,
+            JSON.stringify(defaultCells),
+            'utf-8'
+          );
+          res.send(JSON.stringify(defaultCells));
         } else {
           throw err;
         }
